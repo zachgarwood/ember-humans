4e30ec5b155cbe6a4f69e759e76ec69196a13f0a
Zach Garwood <zachgarwood@gmail.com>
2019-03-03 20:00:43 -0600

    Reset rootURL

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
-    rootURL: '/ember-humans',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
